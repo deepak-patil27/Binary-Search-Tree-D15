4cb7b9111f53ff912b4422e1b2e4fcc8912d964f
deepak-patil27 <mr.deepak.patil@outlook.com>
2022-07-29 18:46:57 +0530

    Added: Welcome To Binary Search Tree

---
+++ src/com/bridgelab/BinaryTreeMain.java
@@ -0,0 +1,8 @@
+package com.bridgelab;
+
+public class BinaryTreeMain {
+	public static void main(String[] args) {
+		System.out.println("     Welcome to Data Structure : Binary Search Tree");
+	}
+
+}
